13bc185d8168524fdf2ee6695fd980fa19d410d4
kashishjivani <kashishjivani2002@gmail.com>
2023-12-19 13:39:31 +0530

    modified routes names

--- controllers/batchController.js
+++ controllers/batchController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../db");
 
-router.get("/api/generateBatch", (req, res) => {
+router.get("/api/fetchBatches", (req, res) => {
   const date = new Date();
   const month = date.getMonth() + 1;
   const query = `SELECT * FROM batches WHERE MONTH(starttime) = ${month}`;
@@ -21,5 +21,23 @@ router.get("/api/generateBatch", (req, res) => {
 
 // (0 0  1 *  *, "/api/generateBatch") -- CRON JOB scheduler
 //  M H DM M DW
+
+router.post("/api/generateBatches", (req, res) => {
+  const { a, b, c, d } = req.body;
+  const query =
+    "INSERT INTO batches (starttime, endtime) VALUES (?, ?), (?, ?), (?, ?), (?, ?)";
+  const values = [[a, b], [c, d], [], []];
+  connection.query(query, values.flat(), (error, results) => {
+    if (error) {
+      console.error("Error inserting data into MySQL:", error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+
+    console.log("Batches data inserted successfully");
+    res.status(201).json({
+      message: "Batches data inserted successfully",
+    });
+  });
+});
 
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,21 +4,22 @@ const connection = require("../db");
 
 router.post("/api/user", (req, res) => {
   const userData = req.body;
+  const { firstName, lastName, age, contactNo, email } = userData;
 
-  if (!userData.firstName) {
+  if (!firstName) {
     return res.status(400).json({ error: "First Name is required" });
-  } else if (userData.age < 18 && userData.age > 65) {
+  } else if (age < 18 && age > 65) {
     return res.status(400).json({ error: "Age should be between 18 and 65" });
   }
 
   const insertQuery =
     "INSERT INTO users (firstName, lastName, age, contactNo, email) VALUES (?, ?, ?, ?, ?)";
   const values = [
-    userData.firstName,
-    userData.lastName,
-    userData.age,
-    userData.contactNo,
-    userData.email,
+    firstName,
+    lastName,
+    age,
+    contactNo,
+    email,
   ];
 
   connection.query(insertQuery, values, (error, results) => {
@@ -31,7 +32,7 @@ router.post("/api/user", (req, res) => {
     res
       .status(201)
       .json({
-        message: "User data inserted successfully",
+        message: "Registered Successfully!",
         userID: results.insertId,
       });
   });
